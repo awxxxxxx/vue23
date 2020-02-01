f5f908ca20d2bc85d4db7108213a444849885893
awbear <13192872690@163.com>
2020-02-01 20:27:31 +0800

    Break down convertHook into convertObjectMethod, convertObjectProperty

--- lib/transformers/lifecycle.ts
+++ lib/transformers/lifecycle.ts
@@ -1,5 +1,6 @@
 import { NodePath } from "@babel/traverse"
 import * as t from '@babel/types';
+import { rename } from "fs";
 
 export enum Hooks {
   setup = 'setup',
@@ -40,22 +41,65 @@ export const Vue23HooksMap: {[index: string]: Hooks} = {
 }
 
 
+
 export function convertHook(path: NodePath<t.ObjectMethod | t.ObjectProperty>) {
-  const name = Vue23HooksMap[path.node.key.name];
-  if (name !== Hooks.setup) {
-    path.node.key.name = name;
-  } else {
-    // TODO remove all path
-    path.remove();
+  if (isDeprecatedHook(path.node.key.name)) {
+    return convertDeprcatedHook(path)
   }
+  renameHook(path.node.key);
   let node;
   if (path.isObjectMethod()) {
-    node = t.arrowFunctionExpression(path.node.params, path.node.body);
-  } else if (path.isObjectProperty() && path.isFunctionExpression(path.node.value)) {
-    const value = path.node.value as t.FunctionExpression;
-    node = t.arrowFunctionExpression(value.params, value.body);
+    node = convertObjectMethodHook(path)
   } else {
-    node = t.arrowFunctionExpression([], t.blockStatement([]));
+    node = convertObjectPropertyHook(path as NodePath<t.ObjectProperty>);
   }
-  // return t.expressionStatement(t.callExpression(path.node.key, [node]))
+  let exps: t.ExpressionStatement[] = []
+  if (node) {
+    exps = [t.expressionStatement(t.callExpression(path.node.key, [node]))];
+  }
+  path.remove();
+  return exps;
+}
+
+function renameHook(key: t.Identifier) {
+  const name = Vue23HooksMap[key.name];
+  key.name = name;
+}
+
+function isDeprecatedHook(name: string) {
+  return ['beforeCreate', 'created'].includes(name);
+}
+
+function convertDeprcatedHook(path: NodePath<t.ObjectMethod | t.ObjectProperty>) {
+  let body: t.Statement[] = []
+  if (path.isObjectMethod()) {
+    body = path.node.body.body;
+  }
+  if (path.isObjectProperty() && t.isFunctionExpression(path.node.value)) {
+    body = path.node.value.body.body;
+  }
+  path.remove();
+  return body;
+}
+
+function convertObjectMethodHook(path: NodePath<t.ObjectMethod>) {
+  return t.arrowFunctionExpression(path.node.params, path.node.body);
+}
+
+function convertObjectPropertyHook(path: NodePath<t.ObjectProperty>) {
+  if (t.isFunctionExpression(path.node.value)) {
+    const value = path.node.value;
+    return t.arrowFunctionExpression(value.params, value.body);
+  }
+  return null;
+}
+
+export function generateSetupNode<T>(path: NodePath<T>) {
+  const parent = path.parentPath;
+  if (parent.isObjectExpression()) {
+    const s = t.objectMethod('method', t.identifier('setup'), [], t.blockStatement([]))
+    parent.node.properties.push(s);
+    return s;
+  }
+  return null;
 }
