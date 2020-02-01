0c467e5513f584c5bbf137ea6994dab3c4c15470
awbear <13192872690@163.com>
2020-02-01 20:25:04 +0800

    Check block statement return

--- lib/transformers/utils.ts
+++ lib/transformers/utils.ts
@@ -12,3 +12,32 @@ export function resolveTopIdentifier(name: any, node: NodePath) {
   }
   return n?.path;
 }
+
+export function hasReturnStatement(bs: t.Statement | null, depth: number, maxDepth = 2) {
+  if (depth > maxDepth) {
+    return false;
+  }
+  if (bs === null) {
+    return false;
+  }
+  if (t.isBlockStatement(bs)) {
+    bs.body.forEach(it => {
+      if (t.isReturnStatement(it)) {
+        return true;
+      }
+      if (t.isIfStatement(it)) {
+        return hasReturnStatement(it.consequent, depth + 1) || hasReturnStatement(it.alternate, depth + 1)
+      }
+    })
+  }
+  return false;
+}
+
+export function findIndexOfReturnStatement(ss: t.Statement[]) {
+  for (let i = 0; i < ss.length; i++) {
+    if (hasReturnStatement(ss[i], 1)) {
+      return i;
+    }
+  }
+  return -1;
+}
